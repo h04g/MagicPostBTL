332a2e537f864ae3ecd02903d3e821267b354417
Hoang Trung Kien <81230694+trungkien20020428@users.noreply.github.com>
2023-12-03 21:16:09 +0700

    delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './page/Auth/Login';
 import './assets/css/auth.css';
 import Register from './page/Auth/Register/Index';
-import HomeBuyer from './page/Buyer/LandingPage/LandingPage';
 import LoginWithSocial from './common/Components/Auth/LoginWithSocial';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import ForgotPassword from './page/Auth/ForgotPassword/Index';
-import HomeSeller from './page/Seller';
 import { Provider } from 'react-redux';
 import { persistor, store } from './Store';
-import ProductDetails from './page/Buyer/ProductDetail/ProductDetails';
 import { PersistGate } from 'redux-persist/integration/react';
-import Cart from './page/Cart/Cart';
-import Payment from './page/Cart/Payment/Payment';
-import Profile from './page/Profile/Profile';
-import Category from './page/Buyer/Category/Category';
-import Search from './page/Buyer/Search/Search';
-import AllProducts from './page/Buyer/AllProduct/AllProducts';
-import PrivateRoute from "./common/CustomRouter/AuthRouter";
-import ProductByStore from "./page/Buyer/ProductByStore/ProductByStore";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomeBuyer />,
+    element: <div>lading</div>
   },
   {
     path: '/Login',
@@ -44,43 +33,6 @@ const router = createBrowserRouter([
     path: 'forgotPassword',
     element: <ForgotPassword />,
   },
-
-  {
-    path: 'seller',
-    element: <HomeSeller />,
-  },
-  {
-    path: 'Product/:id',
-    element: <ProductDetails />,
-  },
-  {
-    path: 'cart',
-    element: <Cart />,
-  },
-  {
-    path: 'payment',
-    element: <PrivateRoute><Payment /></PrivateRoute>,
-  },
-  {
-    path: 'profile',
-    element: <PrivateRoute><Profile /></PrivateRoute> ,
-  },
-  {
-    path: 'category/:id',
-    element: <Category />,
-  },
-  {
-    path: 'search/:keyword',
-    element: <Search />,
-  },
-  {
-    path: 'all',
-    element: <AllProducts />,
-  },
-  {
-    path:'store/:id',
-    element:<ProductByStore />
-  }
 ]);
 
 
