ecae83a48998c14911e6ba55300ba7c4aa8ee8d5
Tran Hoang Huy <114356387+h04g@users.noreply.github.com>
2023-12-18 23:16:35 +0700

    first UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,102 +1,49 @@
 import React from 'react';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './page/Auth/Login';
-import './assets/css/auth.css';
-import Register from './page/Auth/Register/Index';
-import HomeBuyer from './page/Buyer/LandingPage/LandingPage';
-import LoginWithSocial from './common/Components/Auth/LoginWithSocial';
+
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import ForgotPassword from './page/Auth/ForgotPassword/Index';
-import HomeSeller from './page/Seller';
-import { Provider } from 'react-redux';
-import { persistor, store } from './Store';
-import ProductDetails from './page/Buyer/ProductDetail/ProductDetails';
-import { PersistGate } from 'redux-persist/integration/react';
-import Cart from './page/Cart/Cart';
-import Payment from './page/Cart/Payment/Payment';
-import Profile from './page/Profile/Profile';
-import Category from './page/Buyer/Category/Category';
-import Search from './page/Buyer/Search/Search';
-import AllProducts from './page/Buyer/AllProduct/AllProducts';
-import PrivateRoute from "./common/CustomRouter/AuthRouter";
-import ProductByStore from "./page/Buyer/ProductByStore/ProductByStore";
+
+import HomePage from './page/HomePage/HomePage';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import TransactionPoint from './page/TransactionPoint/TransactionPoint';
+import GatheringPoint from './page/GatheringPoint/GatheringPoint';
+import Status from './page/Status/Status';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomeBuyer />,
+    element: <HomePage />,
   },
   {
-    path: '/Login',
-    element: <Login />,
+    path: '/trans',
+    element: <TransactionPoint />,
   },
   {
-    path: '/o2auth',
-    element: <LoginWithSocial />,
+    path: '/gather',
+    element: <GatheringPoint />,
   },
   {
-    path: 'register',
-    element: <Register />,
+    path: '/status',
+    element: <Status />,
   },
-  {
-    path: 'forgotPassword',
-    element: <ForgotPassword />,
-  },
-
-  {
-    path: 'seller',
-    element: <HomeSeller />,
-  },
-  {
-    path: 'Product/:id',
-    element: <ProductDetails />,
-  },
-  {
-    path: 'cart',
-    element: <Cart />,
-  },
-  {
-    path: 'payment',
-    element: <PrivateRoute><Payment /></PrivateRoute>,
-  },
-  {
-    path: 'profile',
-    element: <PrivateRoute><Profile /></PrivateRoute> ,
-  },
-  {
-    path: 'category/:id',
-    element: <Category />,
-  },
-  {
-    path: 'search/:keyword',
-    element: <Search />,
-  },
-  {
-    path: 'all',
-    element: <AllProducts />,
-  },
-  {
-    path:'store/:id',
-    element:<ProductByStore />
-  }
 ]);
-
 
 const LoadingPage = () => {
   return <div>Loading</div>;
 };
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={LoadingPage} persistor={persistor}>
-        <div>
-          <ToastContainer />
-          <RouterProvider router={router} />
-        </div>
-      </PersistGate>
-    </Provider>
+    <>
+      <Header />
+      <div>
+        <ToastContainer />
+        <RouterProvider router={router} />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-dark text-light">This is Footer</div>
+    </>
+  );
+};
+
+export default Footer;
